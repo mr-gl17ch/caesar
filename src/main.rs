4aeb9df9edee1247d5957bb73a5159bcaaa535c6
mr-gl17ch <wizard101master2010@hotmail.co.uk>
2022-02-28 01:49:49 +0000

    updated main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,54 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let text = &args[1];
-    //let shift_text = String::new();
-    let mut shift = args[2]
-        .trim()
-        .parse::<isize>()
-        .expect("error: please input a  number as second arg");
+    if args.len() != 3 {
+        println!("error: please use two args");
+    } else {
+        let text = &args[1];
+        let shift = args[2]
+            .trim()
+            .parse::<isize>()
+            .expect("error: please input a  number as second arg");
+        let cipher = shift_text(text, shift);
+        println!("{}", cipher);
+    }
+}
+fn shift_text(text: &String, shift: isize) -> String {
     const ALPHA: &str = "abcdefghijklmnopqrstuvwxyz";
     const SHIFT_MAX: isize = 25;
-    const SHIFT_MIN: isize = -26;
-    'charloop: for mut chr in text.chars() {
+    const _SHIFT_MIN: isize = -26;
+    let mut text_to_shift = String::new();
+    for mut chr in text.chars() {
         if chr.is_alphanumeric() {
             chr.make_ascii_lowercase();
-            for (i, al) in ALPHA.chars().enumerate() {
-                println!("{} {} {} {}", chr, al, i, shift);
-                if chr == al {
-                    while ((shift + i as isize) > SHIFT_MAX) || (shift + i as isize) < SHIFT_MIN {
+            if shift.is_positive() {
+                for (i, al) in ALPHA.chars().enumerate() {
+                    if chr == al {
                         if shift + i as isize > SHIFT_MAX {
-                            println!(
-                                "{}",
-                                ALPHA
-                                    .chars()
-                                    .nth(((shift as usize + i) % SHIFT_MAX as usize) - 1)
-                                    .expect("")
-                            );
-                            continue 'charloop;
-                        } else if shift + i as isize > SHIFT_MIN {
-                            println!(
-                                "{}",
-                                ALPHA
-                                    .chars()
-                                    .nth(((shift as usize + i) % SHIFT_MIN as usize))
-                                    .expect("")
-                            );
+                            if chr != 'z' {
+                                text_to_shift.push(
+                                    ALPHA
+                                        .chars()
+                                        .nth(((shift as usize + i) % SHIFT_MAX as usize) - 1)
+                                        .expect(""),
+                                );
+                            } else {
+                                text_to_shift.push(
+                                    ALPHA
+                                        .chars()
+                                        .nth((shift as usize + i) % SHIFT_MAX as usize)
+                                        .expect(""),
+                                );
+                            }
+                        } else {
+                            println!("{}", ALPHA.chars().nth(shift as usize + i).expect(""));
                         }
                     }
-                    //println!("{}",ALPHA.chars().nth(shift as usize+i).expect("error: could not convert to char"));
                 }
             }
+        } else {
+            text_to_shift.push(chr);
         }
     }
+    text_to_shift
 }
